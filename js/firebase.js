e26f02a84f9c1ac45bb98e1f2a3444420e53bf95
bmys2449bk <bmys2449bk@gmail.com>
2026-06-30 18:17:16 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,10 +1,1 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCTjzsm2jCUJWZyzSldXaJkKZCqT5INQdM",
-  authDomain: "balibalulu-garden.firebaseapp.com",
-  projectId: "balibalulu-garden",
-  storageBucket: "balibalulu-garden.firebasestorage.app",
-  messagingSenderId: "659390075752",
-  appId: "1:659390075752:web:aa850376fbb59e98603cdd",
-  measurementId: "G-Z8N375NWPJ"
-};
+
